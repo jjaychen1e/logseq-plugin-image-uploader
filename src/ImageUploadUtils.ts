0b665d955f39419e3cd9929a51ac1ae6c10680e0
zhangtian <zhangtian@moresec.cn>
2024-06-18 22:18:43 +0800

    feat: support link upload and setting

--- src/ImageUploadUtils.ts
+++ src/ImageUploadUtils.ts
@@ -25,40 +25,85 @@ async function uploadImage(url: string): Promise<string> {
         })
 }
 
+async function fetchResponse(url: string): Promise<string> {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+    const blob = await response.blob();
+    return new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const result = e.target?.result as string;
+            resolve(result);
+        };
+        reader.onerror = (e) => {
+            reject(new Error('FileReader error'));
+        };
+        reader.readAsArrayBuffer(blob);
+    });
+}
+
+async function reUploadImage(url: string, basePath:string): Promise<string> {
+    try {
+        const timestamp = Date.now();
+        const filename = `${timestamp}${url.substring(url.lastIndexOf('.'))}`
+        const path = `${basePath}/${logseq.FileStorage.ctxId}/${filename}`
+        const response = await fetchResponse(url)
+        await logseq.FileStorage.setItem(filename, response)
+        const newURL = await uploadImage(path)
+        await logseq.FileStorage.removeItem(filename)
+        return newURL
+    } catch (error) {
+        console.error('Error:', error);
+        logseq.App.showMsg("Error: " + (error as Error).message, "error");
+        throw error;
+    }
+}
+
 const imageExtensions = [".png", ".jpg", ".jpeg", ".gif", ".tiff", ".tif", ".bmp", ".svg", ".webp"];
 
 export async function checkAndUploadBlock(srcBlock: BlockEntity, graphPath: string) {
     let content = srcBlock.content;
-
-    // match => ![name](url)
+    // match => ![name](url) or https://xxx
     let match;
-    while ((match = /\!\[.*?\]\((.*?)\)/g.exec(content))) {
+    while ((match = /(?:\!\[(.*?)\]\((.*?)\))|(https?:\/\/[^\s]+)/g.exec(content))){
+        let imageURLText: string = "";
+        let imageURL: string  = "";
         content = content.replace(match[0], "");
-        const imageURLText: string = match[1];
-        
-        // `Uploder` <= Historical problem... This typo was fixed in v0.0.8.
-        if (match[0].startsWith("![Replaced by Image Uploader]") || match[0].startsWith("![Replaced by Image Uploder]")) {
-            continue;
+        if (match[2]) {
+            imageURLText = match[1];
+            imageURL = match[2];
+        } else {
+            imageURL = match[3];
         }
 
-        if (!imageExtensions.some((x) => imageURLText.toLowerCase().endsWith(x))) {
-            // Not a image, maybe a PDF file?
+        if (imageURLText.startsWith("Replaced by Image Uploader") || imageURLText.startsWith("Replaced by Image Uploder") || imageURLText.startsWith("Replaced by Image Downloader")) {
+            continue;
+        }
+        if (!imageExtensions.some((x) => imageURL.toLowerCase().endsWith(x))) {
             continue;
         }
 
         let uploadNetworkImage = logseq.settings?.uploadNetworkImage ?? false;
-        if (imageURLText.startsWith("../") || uploadNetworkImage) {
-            const originalURL = imageURLText.startsWith("../") ? graphPath + match[1].replace("../", "/") : match[1];
-            const imageURLRemote = await uploadImage(originalURL);
-            if (imageURLRemote != "" && imageURLRemote != undefined && imageURLRemote != null) {
-                // We need to fetch again, becasue block content may changed.
-                const block = await logseq.Editor.getBlock(srcBlock.uuid);
-                if (block && block.content) {
-                    // This should be executed sequentially to avoid race condition.
-                    // TODO: - Maybe we can put this operation into a seperate dispatch queue to improve performance?
-                    await logseq.Editor.updateBlock(block.uuid, block.content.replace(match[0],  `![Replaced by Image Uploader](${imageURLRemote})`));
-                    await recordUploadedImageFile(match[1]);
-                }
+        let imageURLRemote: string
+        if (imageURL.startsWith("http://") || imageURL.startsWith("https://") ) {
+            if (logseq.settings?.skipURLPrefix && imageURL.startsWith(logseq.settings?.skipURLPrefix))  {
+                continue
+            }
+            imageURLRemote = await reUploadImage(imageURL,`${logseq.settings?.homePath}/.logseq/storages`);
+        } else if (imageURL.startsWith("../") || uploadNetworkImage){
+            const originalURL = imageURL.startsWith("../") ? graphPath + match[1].replace("../", "/") : match[1];
+            imageURLRemote = await uploadImage(originalURL);
+        } else {
+            continue
+        }
+        if (imageURLRemote != "" && imageURLRemote != undefined) {
+            // We need to fetch again, becasue block content may changed.
+            const block = await logseq.Editor.getBlock(srcBlock.uuid);
+            if (block && block.content) {
+                await logseq.Editor.updateBlock(block.uuid, block.content.replace(match[0],  `![Replaced by Image Uploader](${imageURLRemote})`));
+                await recordUploadedImageFile(imageURL);
             }
         }
     }
---
+++ src/settings.ts
@@ -0,0 +1,27 @@
+import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin';
+
+const settings: SettingSchemaDesc[] = [
+    {
+        key: 'uploadNetworkImage',
+        type: 'boolean',
+        title: 'Upload Image To PicGo Force',
+        description: 'force to upload image to PicGo',
+        default: false,
+    },
+    {
+        key: 'homePath',
+        type: 'string',
+        title: 'User Home Path',
+        description: 'Use Home Path, such as /Users/zhang',
+        default: '',
+    },
+    {
+        key: 'skipURLPrefix',
+        type: 'string',
+        title: 'Skip URL Prefix',
+        description: 'skip upload image to PicGo by HTTPS URL prefix',
+        default: 'https://cdn.jsdelivr.net/gh',
+    },
+];
+
+export default settings;
